65b994cb57920078bb6f6ef0ce65b00937075437
DannyBrito <britodanny1997@gmail.com>
2019-10-28 23:18:11 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+
+    @@all = []
+
+
+    def self.all
+        @@all
+    end
+
+    def initialize(attributes)
+        attributes.each {|k,v| instance_variable_set("@#{k}",v)}
+        self.class.all << self
+    end
+    
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+
+    attr_accessor :name, :type, :booty
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all = []
+    end 
+
+    def initialize(attributes)
+        attributes.each {|k,v| instance_variable_set("@#{k}",v)}
+        self.class.all << self
+    end
+
+
 end
