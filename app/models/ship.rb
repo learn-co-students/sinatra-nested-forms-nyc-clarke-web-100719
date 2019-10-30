e91db1136c19b6ffc281f1ddb2fdcab9dd63702d
gnardinosaur <github email address>
2019-10-30 08:47:57 -0400

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+
+    attr_accessor :name, :type, :booty
+
+    @@all = []
+
+    def initialize(args_hash)
+        @name = args_hash[:name]
+        @type = args_hash[:type]
+        @booty = args_hash[:booty]
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all.clear
+    end
+    
 end
